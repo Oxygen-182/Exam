19ff25ad77d3e07bcb29ee3b7b32c0902193e9da
Oxygen-182 <119591922+Oxygen-182@users.noreply.github.com>
2023-03-11 15:05:36 +0300

    Add files via upload

---
+++ src/exam/exam/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace exam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.xValueBox = new System.Windows.Forms.TextBox();
+            this.yValueBox = new System.Windows.Forms.TextBox();
+            this.labelx = new System.Windows.Forms.Label();
+            this.labely = new System.Windows.Forms.Label();
+            this.answerButton = new System.Windows.Forms.Button();
+            this.toolStripStatus = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.menuStrip1.SuspendLayout();
+            this.toolStripStatus.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1052, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(50, 21);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(102, 21);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 42);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1024, 468);
+            this.webBrowser1.TabIndex = 1;
+            // 
+            // xValueBox
+            // 
+            this.xValueBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.xValueBox.Location = new System.Drawing.Point(314, 554);
+            this.xValueBox.Name = "xValueBox";
+            this.xValueBox.Size = new System.Drawing.Size(305, 29);
+            this.xValueBox.TabIndex = 2;
+            // 
+            // yValueBox
+            // 
+            this.yValueBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.yValueBox.Location = new System.Drawing.Point(314, 595);
+            this.yValueBox.Name = "yValueBox";
+            this.yValueBox.Size = new System.Drawing.Size(305, 29);
+            this.yValueBox.TabIndex = 3;
+            // 
+            // labelx
+            // 
+            this.labelx.AutoSize = true;
+            this.labelx.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelx.Location = new System.Drawing.Point(78, 555);
+            this.labelx.Name = "labelx";
+            this.labelx.Size = new System.Drawing.Size(230, 24);
+            this.labelx.TabIndex = 4;
+            this.labelx.Text = "Введите значение для x";
+            // 
+            // labely
+            // 
+            this.labely.AutoSize = true;
+            this.labely.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labely.Location = new System.Drawing.Point(78, 595);
+            this.labely.Name = "labely";
+            this.labely.Size = new System.Drawing.Size(229, 24);
+            this.labely.TabIndex = 5;
+            this.labely.Text = "Введите значение для y";
+            // 
+            // answerButton
+            // 
+            this.answerButton.BackColor = System.Drawing.SystemColors.Highlight;
+            this.answerButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.answerButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.answerButton.Location = new System.Drawing.Point(719, 538);
+            this.answerButton.Name = "answerButton";
+            this.answerButton.Size = new System.Drawing.Size(258, 102);
+            this.answerButton.TabIndex = 6;
+            this.answerButton.Text = "Запустить проверку";
+            this.answerButton.UseVisualStyleBackColor = false;
+            this.answerButton.Click += new System.EventHandler(this.answerButton_Click);
+            // 
+            // toolStripStatus
+            // 
+            this.toolStripStatus.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStripStatus.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1});
+            this.toolStripStatus.Location = new System.Drawing.Point(0, 516);
+            this.toolStripStatus.Name = "toolStripStatus";
+            this.toolStripStatus.Size = new System.Drawing.Size(1052, 25);
+            this.toolStripStatus.TabIndex = 7;
+            this.toolStripStatus.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(96, 22);
+            this.toolStripLabel1.Text = "toolStripLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1052, 541);
+            this.Controls.Add(this.toolStripStatus);
+            this.Controls.Add(this.answerButton);
+            this.Controls.Add(this.labely);
+            this.Controls.Add(this.labelx);
+            this.Controls.Add(this.yValueBox);
+            this.Controls.Add(this.xValueBox);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStripStatus.ResumeLayout(false);
+            this.toolStripStatus.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox xValueBox;
+        private System.Windows.Forms.TextBox yValueBox;
+        private System.Windows.Forms.Label labelx;
+        private System.Windows.Forms.Label labely;
+        private System.Windows.Forms.Button answerButton;
+        private System.Windows.Forms.ToolStrip toolStripStatus;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+    }
+}
+
---
+++ src/exam/exam/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exam
+{
+    public partial class Form1 : Form
+    {
+        double x;
+        double y;
+        string filePath;
+        string fileName;
+        public Form1()
+        {
+            InitializeComponent();
+            xValueBox.Visible = false;
+            yValueBox.Visible = false;
+            answerButton.Visible = false;
+            labelx.Visible = false;
+            labely.Visible = false;
+            toolStripLabel1.Visible = false;
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "HTML Document (*.html)|*.html";
+            if (openFileDialog.ShowDialog() == DialogResult.Cancel) return;
+            filePath = openFileDialog.FileName;
+            fileName = openFileDialog.SafeFileName;
+            webBrowser1.Navigate(filePath);
+            xValueBox.Visible = true;
+            yValueBox.Visible = true;
+            answerButton.Visible = true;
+            labelx.Visible = true;
+            labely.Visible = true;
+            toolStripLabel1.Visible = true;
+            toolStripLabel1.Text = "Строка статуса";
+            this.Size = new Size(1075, 740);
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Автор работы: Родионов Юрий Русланович\nГруппа: ПКсп-120\nВариант: 10");
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void answerButton_Click(object sender, EventArgs e)
+        {
+            if (fileName == "pageWithTask1.html")
+            {
+                if (!double.TryParse(xValueBox.Text, out x))
+                {
+                    MessageBox.Show("В поле ввода координаты X введены неверные данные", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                if (!double.TryParse(yValueBox.Text, out y))
+                {
+                    MessageBox.Show("В поле ввода координаты Y введены неверные данные", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                x = double.Parse(xValueBox.Text);
+                y = double.Parse(yValueBox.Text);
+
+                if (y < Math.Sin(x) && y < 0.5 && x > 0)
+                {
+                    MessageBox.Show("Точка находится внутри заштрихованной области");
+                    toolStripLabel1.Text = "Точка находится внутри заштрихованной области";
+                }
+                else if (y > Math.Sin(x) || y > 0.5 || x < 0)
+                {
+                    MessageBox.Show("Точка находится вне заштрихованной области!");
+                    toolStripLabel1.Text = "Точка находится на границе заштрихованной области";
+                }
+                else
+                {
+                    MessageBox.Show("Точка находится на границе заштрихованной области");
+                    toolStripLabel1.Text = "Точка находится на границе заштрихованной области";
+                }
+            }
+            else if (fileName == "pageWithTask2.html")
+            {
+                if (!double.TryParse(xValueBox.Text, out x))
+                {
+                    MessageBox.Show("В поле ввода координаты X введены неверные данные", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                if (!double.TryParse(yValueBox.Text, out y))
+                {
+                    MessageBox.Show("В поле ввода координаты Y введены неверные данные", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                x = double.Parse(xValueBox.Text);
+                y = double.Parse(yValueBox.Text);
+
+                if (y > 0 && y > (-x / 0.5) + 4 && x > 0 && y < (-x + 6))
+                {
+                    MessageBox.Show("Точка находится внутри заштрихованной области");
+                    toolStripLabel1.Text = "Точка находится внутри заштрихованной области";
+                }
+                else if (y < 0 || y < (-x / 0.5) + 4 || x < 0 || y > (-x + 6))
+                {
+                    MessageBox.Show("Точка находится вне заштрихованной области");
+                    toolStripLabel1.Text = "Точка находится вне заштрихованной области";
+                }
+                else
+                {
+                    MessageBox.Show("Точка находится на границе заштрихованной области");
+                    toolStripLabel1.Text = "Точка находится на границе заштрихованной области";
+                }
+            }
+        }
+    }
+}
